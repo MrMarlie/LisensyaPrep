c2ea7b059e73f98b79c6e5b2d7c90ed877663459
MrNoOne1 <64152811+MrNoOne1@users.noreply.github.com>
2026-05-22 01:28:54 +0800

    Fix Grow script: add data-grow-initializer attr, move to head

--- app/layout.tsx
+++ app/layout.tsx
@@ -67,6 +67,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <script
+          data-grow-initializer=""
+          dangerouslySetInnerHTML={{__html:`!(function(){window.growMe||((window.growMe=function(e){window.growMe._.push(e);}),(window.growMe._=[]));var e=document.createElement("script");(e.type="text/javascript"),(e.src="https://faves.grow.me/main.js"),(e.defer=!0),e.setAttribute("data-grow-faves-site-id","U2l0ZTowNTRkN2Q0Ny0yMGVkLTRmOTYtOWY4MS04MDMzNzJlZjQyNTQ=");var t=document.getElementsByTagName("script")[0];t.parentNode.insertBefore(e,t);})();`}}
+        />
+      </head>
       <body className={`${inter.className} bg-[#080d1b] text-white antialiased`}>
         <Script
           src="https://www.googletagmanager.com/gtag/js?id=G-CP2WTTWKQH"
@@ -86,9 +92,6 @@ export default function RootLayout({
           crossOrigin="anonymous"
           strategy="afterInteractive"
         />
-        <Script id="grow-me" strategy="afterInteractive">
-          {`!(function(){window.growMe||((window.growMe=function(e){window.growMe._.push(e);}),(window.growMe._=[]));var e=document.createElement("script");(e.type="text/javascript"),(e.src="https://faves.grow.me/main.js"),(e.defer=!0),e.setAttribute("data-grow-faves-site-id","U2l0ZTowNTRkN2Q0Ny0yMGVkLTRmOTYtOWY4MS04MDMzNzJlZjQyNTQ=");var t=document.getElementsByTagName("script")[0];t.parentNode.insertBefore(e,t);})();`}
-        </Script>
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
